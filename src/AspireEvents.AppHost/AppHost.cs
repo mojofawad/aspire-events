25ce84babad26b46cdd1645e5f4c3e9b2655c0ac
mojofawad <mojofawad@gmail.com>
2025-06-04 21:24:09 -0400

    register next app and API services

--- src/AspireEvents.AppHost/AppHost.cs
+++ src/AspireEvents.AppHost/AppHost.cs
@@ -1,5 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.AspireEvents_Gateway>("api-gateway");
 
+builder.AddProject<Projects.AspireEvents_Bookings>("bookings-api");
+builder.AddProject<Projects.AspireEvents_Events>("events-api");
+builder.AddProject<Projects.AspireEvents_Notifications>("notifications-api");
+builder.AddProject<Projects.AspireEvents_Payments>("payments-api");
+builder.AddProject<Projects.AspireEvents_Users>("users-api");
+
+builder.AddNpmApp("web-app", "../aspire-events-web", "dev")
+    .WithExternalHttpEndpoints()
+    .WithHttpEndpoint(env: "PORT");
 
 builder.Build().Run();
